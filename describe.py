f9957f6b40932150a0360cf7eb0128dfd4684551
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-06-12 13:41:11 +0000

    add logging

--- describe.py
+++ describe.py
@@ -2,7 +2,8 @@ import logging
 import pandas as pd
 import sys
 
-logging.basicConfig()
+log_format = "[% (name)s][%(levelname)-6s] %(message) s"
+logging.basicConfig(format=log_format)
 logger = logging.getLogger("describe")
 logger.setLevel(logging.DEBUG)
 
@@ -12,4 +13,4 @@ try:
     df = pd.read_csv(argument)
     print(df.describe())
 except Exception:
-    print("Had a problem trying to read CSV file")
+    logger.exception("Had a problem trying to read CSV file")
