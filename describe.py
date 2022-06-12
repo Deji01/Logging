1c67b43b120195acf05713b55ae28cb7149c60bc
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-06-12 13:48:18 +0000

    setup http-app.py

--- describe.py
+++ describe.py
@@ -5,7 +5,7 @@ import sys
 log_format = "[% (name)s][%(levelname)-6s] %(message) s"
 logging.basicConfig(format=log_format)
 logger = logging.getLogger("describe")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 
 argument = sys.argv[-1]
 logger.debug("processing input file: %s", argument)
