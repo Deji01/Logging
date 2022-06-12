851025d572167064d4dd26314b00cb29af1ff6ed
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-06-12 13:50:40 +0000

    done

--- http-app.py
+++ http-app.py
@@ -10,5 +10,9 @@ root_logger.setLevel(logging.DEBUG)
 #new logger for this script
 logger = logging.getLogger("http-app")
 
+# fine tune the urllib logger:
+urllib_logger = logging.getLogger('urllib3')
+urllib_logger.setLevel(logging.ERROR)
+
 logger.info("About to send a request to example.com")
 requests.get("http://example.com")
